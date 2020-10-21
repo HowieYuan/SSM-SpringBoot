06e41ea6c5ffbfd180cc3063fe1782f3f44f74f3
xuxunbiao123 <biao111>
2020-10-21 09:28:09 +0800

    1

--- src/test/java/com/howie/springbootssm/ApplicationTests.java
+++ src/test/java/com/howie/springbootssm/ApplicationTests.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootSsmApplicationTests {
+public class ApplicationTests {
 	@Resource
 	private SysRegionDao sysRegionDao;
 	@Test
